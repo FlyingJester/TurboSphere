07465c020e6c229c5d48eb486530ef3e65852ae5
FlyingJester <foolkingcrown@gmail.com>
2013-12-17 14:56:15 -0900

    Fixed OpenGL compatibility issue on non-AMD video cards.

--- bin/Release/system/shaders/system.fs
+++ bin/Release/system/shaders/system.fs
@@ -1,5 +1,8 @@
-#version 120
+#version 140
+
+uniform sampler2D textureSampler;
 
 void main(void){
-	gl_FragColor = gl_Color;
+    vec4 texcolor = texture2D(textureSampler, gl_TexCoord[0].st);
+	gl_FragColor = texcolor*gl_Color;
 }
--- bin/Release/system/shaders/system.vs
+++ bin/Release/system/shaders/system.vs
@@ -1,5 +1,7 @@
-#version 120
+#version 140
 
 void main(void){
+    gl_TexCoord[0] = gl_MultiTexCoord0;
+    gl_FrontColor = gl_Color;
 	gl_Position = gl_ModelViewProjectionMatrix*gl_Vertex;
 }
