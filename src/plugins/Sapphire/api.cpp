5ebb00582006560ad0a5c6aa5d2b5cd3087b8fed
FlyingJester <foolkingcrown@gmail.com>
2014-07-15 11:31:12 -0700

    Fixed TSPR version, added SDL_Image abstraction.

--- src/plugins/Sapphire/api.cpp
+++ src/plugins/Sapphire/api.cpp
@@ -1,1 +1,43 @@
 #include "api.hpp"
+#include "Sapphire.hpp"
+
+namespace Sapphire {
+
+// TODO: new T5!
+SDL_Surface *LoadSurface(const char *aPath){
+
+    SDL_Surface *lAnyFormatSurface = IMG_Load(aPath);
+
+    if(lAnyFormatSurface==nullptr){
+        fprintf(stderr, P_ERROR "Could not load Image %s. %s\n", aPath, SDL_GetError());
+        return nullptr;
+    }
+
+    const SDL_Rect lRect = {0, 0, lAnyFormatSurface->w, lAnyFormatSurface->h};
+    SDL_Surface* rCorrectFormatSurface = SDL_CreateRGBSurface(0, lRect->w, lRect->h, IMAGE_DEPTH, CHANNEL_MASKS);
+    assert(rCorrectFormatSurface);
+
+    SDL_LowerBlit(lAnyFormatSurface, &lRect, rCorrectFormatSurface, &lRect);
+
+    SDL_FreeSurface(lAnyFormatSurface);
+
+    return rCorrectFormatSurface;
+}
+
+SDL_Surface *CreateSurface(unsigned aWidth, unsigned aHeight, TS_Color *color){
+    const uint32_t Color = color->toInt();
+
+    SDL_Surface* rSurface = SDL_CreateRGBSurface(0, aWidth, aHeight, IMAGE_DEPTH, CHANNEL_MASKS);
+    assert(rSurface);
+
+    SDL_LockSurface(   rSurface );
+    {
+        uint32_t *lPixels = (uint32_t *)(rSurface->pixels);
+        std::fill(lPixels, &(lPixels[aWidth*aHeight]), Color);
+    }
+    SDL_UnlockSurface( rSurface );
+
+    return rSurface;
+}
+
+}
