c9d7748414be1946dbe8fe70b2ef02988642659e
FlyingJester <foolkingcrown@gmail.com>
2014-07-17 22:02:40 -0700

    Added linear circle approximation and bresenham's from SDL_GL to graphicalg

--- src/graphicalg/structures.cpp
+++ src/graphicalg/structures.cpp
@@ -1,35 +1,26 @@
 #define GRAPHICALG_INTERNAL
 #include "structures.h"
-/*
-TS_Point TS_Point::operator=(const SDL_Rect &c2)
-{
-    // use the Cents constructor and operator+(int, int)
-    return TS_Point(c2.x, c2.y);
-}
-*/
-TS_Point::TS_Point(int _x, int _y){
-    x = _x;
-    y = _y;
+
+TS_Point *TS_CreatePoint(int x, int y){
+  TS_Point *rPoint = new TS_Point();
+  rPoint->x = x;
+  rPoint->y = y;
+  return rPoint;
 }
 
-TS_Point::~TS_Point(){
-
+void TS_DeletePoint(TS_Point *aPoint){
+  delete aPoint;
 }
 
-TS_Segment::TS_Segment(int _x1, int _y1, int _x2, int _y2){
-    x1 = _x1;
-    y1 = _y1;
-    x2 = _x2;
-    y2 = _y2;
+TS_Segment *TS_CreateSegment(int _x1, int _y1, int _x2, int _y2){
+    TS_Segment *rSegment = new TS_Segment();
+    rSegment->x1 = _x1;
+    rSegment->y1 = _y1;
+    rSegment->x2 = _x2;
+    rSegment->y2 = _y2;
+    return rSegment;
 }
 
-TS_Segment::TS_Segment(void){
-    x1 = 0;
-    y1 = 0;
-    x2 = 0;
-    y2 = 0;
-}
-
-TS_Segment::~TS_Segment(){
-
+void TS_DeleteSegment(TS_Segment *aSegment){
+  delete aSegment;
 }
--- src/graphicalg/structures.h
+++ src/graphicalg/structures.h
@@ -1,43 +1,59 @@
-#ifndef STRUCURES_HEAD
-#define STRUCURES_HEAD
+#ifndef GRAPHICALG_STRUCURES_HEAD
+#define GRAPHICALG_STRUCURES_HEAD
 #include <stdint.h>
-#include <cstdio>
-#ifdef _WIN32
-	#ifdef GRAPHICALG_INTERNAL
-		#define GRAPHICALG_EXPORT __declspec(dllexport)
-	#else
-		#define GRAPHICALG_EXPORT __declspec(dllimport)
-	#endif
-#define GRAPHICALGCALL _cdecl
-#else
-	#define GRAPHICALG_EXPORT extern "C"
+#include <stdlib.h>
+
+#ifndef GRAPHICALG_EXPORT
+  #ifdef _WIN32
+    #ifdef GRAPHICALG_INTERNAL
+      #define GRAPHICALG_EXPORT __declspec(dllexport)
+    #else
+      #define GRAPHICALG_EXPORT __declspec(dllimport)
+    #endif
+  #define GRAPHICALGCALL _cdecl
+  #else
+    #define GRAPHICALGCALL
+    #ifdef __cplusplus
+      #define GRAPHICALG_EXPORT extern "C"
+    #else
+      #define GRAPHICALG_EXPORT
+    #endif
+  #endif
 #endif
 
-#ifdef _WIN32
-typedef struct GRAPHICALG_EXPORT TS_Point {
-#else
-GRAPHICALG_EXPORT typedef struct TS_Point {
+#ifndef __cplusplus
+typedef
 #endif
+struct TS_Point {
 
 	int x;
 	int y;
-    TS_Point(int, int);
-    ~TS_Point();
-} TS_Point;
 
-#ifdef _WIN32
-typedef struct GRAPHICALG_EXPORT TS_Segment {
-#else
-GRAPHICALG_EXPORT typedef struct TS_Segment {
+}
+#ifndef __cplusplus
+TS_Point
 #endif
+;
+
+#ifndef __cplusplus
+typedef
+#endif
+struct TS_Segment {
 
 	int x1;
 	int y1;
 	int x2;
 	int y2;
-    TS_Segment(int, int, int, int);
-    TS_Segment();
-    ~TS_Segment();
-} TS_Segment;
+
+}
+#ifndef __cplusplus
+TS_Segment
+#endif
+;
+
+GRAPHICALG_EXPORT TS_Point *  GRAPHICALGCALL TS_CreatePoint(int x, int y);
+GRAPHICALG_EXPORT void        GRAPHICALGCALL TS_DeletePoint(TS_Point *aPoint);
+GRAPHICALG_EXPORT TS_Segment *GRAPHICALGCALL TS_CreateSegment(int _x1, int _y1, int _x2, int _y2);
+GRAPHICALG_EXPORT void        GRAPHICALGCALL TS_DeleteSegment(TS_Segment *aSegment);
 
 #endif
