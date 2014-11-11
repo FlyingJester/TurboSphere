019599c0eec7ab6fbf7a9a2b671d32138c0707fd
FlyingJester <foolkingcrown@gmail.com>
2014-11-11 05:04:06 -0900

    Simplified script loader

--- src/configmanager/openscript.h
+++ src/configmanager/openscript.h
@@ -17,7 +17,7 @@ using namespace std;
 #endif
 
 
-CONFIGMGR_EXPORT std::string openfile(const char *Rfile);
+CONFIGMGR_EXPORT char *openfile(const char *Rfile);
 CONFIGMGR_EXPORT bool ExecuteString(v8::Handle<v8::String> source, v8::Handle<v8::String> name, v8::Isolate *isolate, bool print_result);
 CONFIGMGR_EXPORT void TS_LoadScript(const v8::FunctionCallbackInfo<v8::Value>& args);
 CONFIGMGR_EXPORT void TS_LoadSystemScript(const v8::FunctionCallbackInfo<v8::Value>& args);
