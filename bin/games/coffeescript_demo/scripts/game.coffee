e15bfa2f9ecf5f53ce639c1c7745408f44b22357
FlyingJester <foolkingcrown@gmail.com>
2014-11-17 22:44:22 -0900

    Added CoffeeScript demo

---
+++ bin/games/coffeescript_demo/scripts/game.coffee
@@ -0,0 +1,45 @@
+
+class Character
+  constructor: (@name, @x, @y) ->
+    @image = new Image(new Surface("#{@name}.png"))
+    @shape = new Shape([{x:0, y:0}, {x:32, y:0}, {x:32, y:32}, {x:0, y:32}], @image)
+    @group = new Group(@shape, GetDefaultShaderProgram())
+
+trevor = new Character("trevor", 16, 16)
+dylan  = new Character("dylan", 64, 64)
+
+frame = () ->
+    trevor.group.draw()
+    dylan.group.draw()
+    FlipScreen()
+
+coffee_game.AI = AI = (opt, other) ->
+
+    if opt.x == other.x and opt.y == other.y
+        return
+
+    if opt.x - other.x == 0
+        if(opt.y > other.y)
+            opt.y-= coffee_game.speed / (Math.PI)
+        else
+            opt.y+= coffee_game.speed / (Math.PI)
+    else
+        angle = Math.atan2(-(opt.y - other.y), -(opt.x - other.x))
+        opt.x += Math.cos(angle) * coffee_game.speed / (Math.PI)
+        opt.y += Math.sin(angle) * coffee_game.speed / (Math.PI)
+
+coffee_game.main = main = () ->
+    loop
+        t = GetSeconds()
+        frame()
+        coffee_game.Control(trevor.group)
+
+        coffee_game.AI(dylan.group, trevor.group)
+
+        coffee_game.Bound(trevor.group)
+        coffee_game.Bound(dylan.group)
+
+        Delay(Math.max(GetSeconds()-t+(1/60), 0)*1000)
+
+        break if ShouldQuit()
+    Abort(dylan.group.x)
---
+++ bin/games/coffeescript_demo/scripts/game.js
@@ -0,0 +1,61 @@
+RequireScript("coffee-script.js");
+RequireSystemScript("turbo/bytearray.js");
+
+var coffee_file = new RawFile("../scripts/game.coffee");
+var coffee_src  = coffee_file.read(coffee_file.size);
+var coffee_byteview = new Uint8Array(coffee_src);
+var coffee_bytearray = ByteArrayFromTypedArray(coffee_byteview);
+
+function ShouldQuit(){return IsKeyPressed(KEY_Q);}
+
+var coffee_game = {speed:2};
+
+coffee_game.Control = function(item){
+
+    var dirs = 0;
+    if(IsKeyPressed(KEY_LEFT))
+        dirs++;
+    if(IsKeyPressed(KEY_RIGHT))
+        dirs++;
+    if(IsKeyPressed(KEY_UP))
+        dirs++;
+    if(IsKeyPressed(KEY_DOWN))
+        dirs++;
+
+    var speed = this.speed;
+
+    switch(dirs){
+    case 1:
+        speed = this.speed;
+        break;
+    case 2:
+        speed = Math.sqrt(this.speed);
+        break;
+    case 3:
+        speed = this.speed;
+        break;
+    case 4:
+        return;
+    }
+
+    if(IsKeyPressed(KEY_LEFT))
+        item.x-=speed;
+    if(IsKeyPressed(KEY_RIGHT))
+        item.x+=speed;
+    if(IsKeyPressed(KEY_UP))
+        item.y-=speed;
+    if(IsKeyPressed(KEY_DOWN))
+        item.y+=speed;
+}
+
+coffee_game.Bound = function(item){
+    item.x = Math.max(0, item.x);
+    item.y = Math.max(0, item.y);
+    item.x = Math.min(GetScreenWidth()-32, item.x);
+    item.y = Math.min(GetScreenHeight()-32, item.y);
+}
+
+function game(){
+    eval(CoffeeScript.compile(CreateStringFromByteArray(coffee_bytearray)));
+    coffee_game.main();
+}
