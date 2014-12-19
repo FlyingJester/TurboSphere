a3be8da78b9e6e3c3297535065f4cbe5aadd9242
FlyingJester <foolkingcrown@gmail.com>
2014-12-18 15:06:04 -0900

    Removed silliness.

--- src/engine/engine.h
+++ src/engine/engine.h
@@ -1,51 +1,14 @@
 #ifndef ENGINE_HEAD
 #define ENGINE_HEAD
-//extern v8::Persistent<v8::Context> context;
 
 #define V8FLAGS "--harmony_typed_arrays"
 
 #include <string>
 #include <string.h>
 #include <math.h>
-#define V8_ALLOW_ACCESS_TO_RAW_HANDLE_CONSTRUCTOR
-#define V8_USE_UNSAFE_HANDLES
 
 #include <t5.h>
 #include <v8.h>
-#if defined _WIN32
-
-    //File handling generalization.
-    #include <windows.h>
-
-    typedef HANDLE filehandle;
-    typedef WIN32_FIND_DATA filedata;
-    #define FILENAME(NAME) NAME.cFileName
-    #define ISDIRECTORY(NAME) (NAME.dwFileAttributes&FILE_ATTRIBUTE_DIRECTORY)
-#endif
-
-#ifndef _WIN32
-
-    //File handling generalization.
-    #include <dirent.h>
-
-    typedef DIR * filehandle;
-    typedef struct dirent * filedata;
-    #define FILENAME(NAME) NAME->d_name
-    #define ISDIRECTORY(NAME) (NAME->d_type==DT_DIR)
-
-#endif
-
-#undef V8_ALLOW_ACCESS_TO_RAW_HANDLE_CONSTRUCTOR
-#undef V8_USE_UNSAFE_HANDLES
-
-
-
-#if defined _WIN32
-
-#else
-
-
-#endif
 
 #include <opengame.h>
 
