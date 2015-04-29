acb4653a07284c86bb521a202714f68df5064d9a
FlyingJester <foolkingcrown@gmail.com>
2015-04-29 15:02:53 -0800

    Changed GC behaviour to be nicer with SM 40

--- src/plugins/Chrono/script.cpp
+++ src/plugins/Chrono/script.cpp
@@ -6,13 +6,35 @@ namespace Chrono {
     bool Delay(JSContext *ctx, unsigned argc, JS::Value *vp){
         JS::CallArgs args = CallArgsFromVp(argc, vp);
         const Turbo::JSType signature[] = {Turbo::Number};
+        
+        static unsigned long long last_gc_second = TS_Chrono_GetTime(TS_Chrono_Millisecond)/1000;
+        
         if(!Turbo::CheckSignature<1>(ctx, args, signature, __func__))
             return false;
         
-        double value = args[0].toNumber();
-        unsigned long nanoseconds = value*1000000.0f;
+        long long nanoseconds = args[0].toNumber()*1000000.0;
+        if(nanoseconds==0ll)
+            return true;
         
-        TS_Chrono_Sleep(ctx, nanoseconds, TS_Chrono_Nanosecond);
+        const unsigned long long c_second = TS_Chrono_GetTime(TS_Chrono_Millisecond)/1000;
+        
+        if((last_gc_second!=c_second) && nanoseconds>10000ull){
+            unsigned long long gc_diff = TS_Chrono_GetTime(TS_Chrono_Nanosecond);
+            
+            if(nanoseconds>1000000ull){
+                JS_GC(JS_GetRuntime(ctx));
+            }
+            else{
+                JS_MaybeGC(ctx);
+            }
+            
+            last_gc_second = c_second;
+            nanoseconds-=(TS_Chrono_GetTime(TS_Chrono_Nanosecond)-gc_diff);
+        }
+        
+        if(nanoseconds){
+            TS_Chrono_Sleep(ctx, nanoseconds, TS_Chrono_Nanosecond);
+        }
         
         return true;
     }
